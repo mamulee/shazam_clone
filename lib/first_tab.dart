e796f3ce645fb16eb20b406ffcb431140056fad1
mamulee <ghdah7@gmail.com>
2023-12-15 01:07:55 +0900

    first page done

--- lib/first_tab.dart
+++ lib/first_tab.dart
@@ -39,6 +39,181 @@ class FirstTab extends StatelessWidget {
       },
     ];
 
-    return Center(child: Text('첫번째 페이지'));
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Icon(Icons.settings),
+              ),
+              Text(
+                "라이브러리",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(null),
+            ],
+          ),
+          SizedBox(height: 18),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                ImageIcon(
+                  NetworkImage("https://i.ibb.co/hxNbZ8p/shazam.png"),
+                  size: 18,
+                ),
+                SizedBox(width: 12),
+                Text(
+                  "Shazam",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.person,
+                ),
+                SizedBox(width: 12),
+                Text(
+                  "아티스트",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.music_note,
+                ),
+                SizedBox(width: 12),
+                Text(
+                  "회원님을 위한 재생목록",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                Text(
+                  "최근 Shazam",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: songs.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 5,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return songGrid(songs[0]['imageUrl']!, songs[0]['title']!,
+                    songs[0]['artist']!);
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
+}
+
+Widget songGrid(String imageUrl, String title, String artist) {
+  return Container(
+    margin: EdgeInsets.only(
+      left: 4,
+      right: 4,
+      top: 4,
+      bottom: 4,
+    ),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          blurRadius: 1,
+          spreadRadius: 1,
+        ),
+      ],
+    ),
+    child: Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(artist),
+                SizedBox(
+                  height: 100,
+                ),
+                Image.network(
+                  "https://i.ibb.co/KG9m5QS/applemusic.png",
+                  width: 60,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }
